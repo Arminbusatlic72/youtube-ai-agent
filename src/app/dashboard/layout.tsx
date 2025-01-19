358b1ab5b000d1c1a59cc2447da886fae17f8522
Armin <busatar@cronos.be>
2025-01-19 23:52:22 +0100

    fixing clerk authentication by making new project, creating dashboard page and layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,5 @@
+export default function LayoutDashboard({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { title } from "process";
 
 export default function LandingPage() {
   return (
@@ -32,6 +32,42 @@ export default function LandingPage() {
             </button>
           </Link>
         </SignedIn>
+        <SignedOut>
+          <SignInButton
+            mode="modal"
+            fallbackRedirectUrl={"/dashboard"}
+            forceRedirectUrl={"/dashboard"}
+          >
+            <button className="group relative inline-flex items-center justify-center px-8 py-3.5 text-base font-medium text-white bg-gradient-to-r from-gray-900 to-gray-800 rounded-full hover:from-gray-800 hover:to-gray-700 transition-all duration-200 shadow-lg hover:shadow-xl hover:translate-y-0.5">
+              Sign up
+              <ArrowRight className="ml-2 h-5 w-5 transition-transform group-hover:translate-x-0.5" />
+              <div className="absolute inset-0 z-10 bg-[linear-gradient(to-right,#e5e5e5_1px,transparent_1px),linear-gradient(to-bottom,#e5e5e5_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+            </button>
+          </SignInButton>
+        </SignedOut>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16 pt-8 max-w-3xl mx-auto">
+          {[
+            {
+              title: "Fast",
+              description: " Real time streamed response"
+            },
+            {
+              title: "Modern",
+              description: "NextJS 15, Tailwind CSS, Convex, Clerk"
+            },
+            {
+              title: "Smart",
+              description: "Powered by your favorite LLM"
+            }
+          ].map(({ title, description }) => (
+            <div key={title} className="text-center">
+              <div className="text-2xl font-semibold text-gray-900">
+                {title}
+              </div>
+              <div className="text-small  text-gray-600">{description}</div>
+            </div>
+          ))}
+        </div>
       </section>
     </main>
   );
